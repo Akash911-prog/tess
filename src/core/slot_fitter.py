5ad05c3c5a097dbebeb224b83232483edefaee43
Akash <Akashsamanta0571@gmail.com>
2025-12-30 20:49:04 +0530

    added basic structure of slotFlitter

---
+++ src/core/slot_fitter.py
@@ -0,0 +1,12 @@
+
+class SlotFitter:
+    def __init__(self):
+        pass
+
+    def is_needed(self, query) -> bool:
+        # TODO: add logic later for slot fitter
+        return False
+    
+    def fit(self, query, context) -> dict[str, str]:
+        # TODO: add logic later for slot fitter
+        return {}
